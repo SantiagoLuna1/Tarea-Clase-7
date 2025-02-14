d785f5d4ccf8c2ddc65401549e010381900c1b28
SantiagoLuna1 <santiluna010@gmail.com>
2025-02-13 22:33:24 -0300

    Tarea 7
    Subir la tarea 7 en main.js y prueba.js

--- js/pruebas.js
+++ js/pruebas.js
@@ -1,15 +1,50 @@
 function probarValidarNombre() {
-  console.assert(
-      validarNombre('') === 'Este campo debe tener al menos 1 caracter',
-      'Validar nombre no validó que el nombre no sea vacío',
-  );
+    console.assert(
+        validarNombre('') === 'Este campo debe tener al menos 1 caracter', //devuelve T o F
+        'Validar nombre no validó que el nombre no sea vacío', //el mensaje de error solo lo da si es F
+    );
 
-  console.assert(
-      validarNombre(
-          '111111111111111111111111111111111111111111111111111111111111111111111111111111111111111') ===
-      'Este campo debe tener menos de 50 caracteres',
-      'Validar nombre no validó que el nombre sea menor a 50 caracteres',
-  );
+    console.assert(
+        validarNombre(
+            '111111111111111111111111111111111111111111111111111111111111111111111111111111111111111') ===
+        'Este campo debe tener menos de 50 caracteres',
+        'Validar nombre no validó que el nombre sea menor a 50 caracteres',
+    );
+
+    console.assert(
+        validarNombre("Santiago") ===  '',
+        "Validar nombre falló con un nombre válido"
+    );
 }
 
-probarValidarNombre();
+function probarValidarCiudad() {
+    console.assert(
+        validarCiudad("Buenos Aires") === '',
+        "Validar ciudad no funcionó con una ciudad válida"
+    );
+
+    console.assert(
+        validarCiudad("") === "El campo ciudad no puede estar vacío",
+        "Validar ciudad no mostró un error cuando la ciudad es vacía"
+    )
+}
+
+function probarValidarDescripcionRegalo() {
+
+    console.assert(
+        validarDescripcionRegalo('') === 'El campo descripcion no puede estar vacío',
+        "La función descripción regalo no validó que el campo no esté vacío"
+    );
+
+    console.assert(
+        validarDescripcionRegalo('asjhdksahdjklhsadoiusadiosadksdamnopkvopxipajrpinrpainspinsafpsfasadkljslkadjlksajdksda') ===
+        'El campo descripcion es muy largo',
+        "La función validar descripción regalo no validó que el campo no sea muy largo"
+    );
+
+    console.assert(
+        validarDescripcionRegalo("regalo") === "",
+        "La función validar descripción regalo no funcionó con una descripción correcta"
+    );
+
+}
